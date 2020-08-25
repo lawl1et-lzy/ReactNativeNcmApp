fb1cabb4f596a650b8ab9084ee940be4752ba985
linzhiyong <zy.lin@trip.com>
2020-08-25 12:49:06 +0800

    1. 初始化 service 2. 添加 qs

---
+++ ReactNativeNcmApp/src/services/login.api.js
@@ -0,0 +1,17 @@
+import config from './config';
+import qs from 'qs';
+export default class Login {
+  // 密码登录
+  static async loginWithPassword({
+    phone = 13052028437,
+    password = '~Museum24',
+  } = {}) {
+    try {
+      const queryParams = qs.stringify({phone, password});
+      let response = await fetch(
+        `${config.baseUrl}/login/cellphone?${queryParams}`,
+      );
+      return await response.json();
+    } catch (error) {}
+  }
+}
